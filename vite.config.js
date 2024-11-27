10fc5a6bf2a8aa08fe70545f3cd91de45fcde584
Madhur Verma <vmadhur345@gmail.com>
2024-11-27 15:22:54 +0530

    hehe 2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: true, // Ensures the server is accessible externally (0.0.0.0)
+    strictPort: true, // Ensures Vite uses the exact port specified (no random port selection)
+    port: process.env.PORT || 3000, // Uses the PORT from Render environment or defaults to 3000
+  },
+});
